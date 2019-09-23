f6228079b3736874a5fd2c7cd28de88c2f25a08d
Saurabh Vishwakarma <saurabhvk@virtusa.com>
2019-09-23 11:50:52 +0530

    get API for users

---
+++ controller/user-controller.js
@@ -0,0 +1,47 @@
+const connection = require('./../config');
+
+module.exports.register = function(req,res){
+   var users = {
+      "first_name": req.body.first_name,
+      "last_name": req.body.last_name,
+      "email_id": req.body.email_id,
+      "contact_number": req.body.contact_number,
+      "role": req.body.role
+   }
+
+   connection.query('INSERT INTO user SET ?',users,function(error,results,fields){
+      if (error){
+         res.json({
+            status: false,
+            message: 'There is some error in query !!!'
+         })
+      } else {
+         res.json({
+            status: true,
+            data: results,
+            message: 'User registration request is generated ...'
+         })
+      }
+   });
+
+}
+
+module.exports.getByUserRole = function(req,res){
+   console.log(JSON.stringify(req.headers));
+   var role = req.header('UserRole')
+
+   connection.query('Select * FROM User WHERE role = ?',role,function(error,results,fields){
+      if (error){
+         res.json({
+            status: false,
+            message: 'There is some error in query !!!'
+         })
+      } else {
+         res.json({
+            status: true,
+            data: results
+
+         })
+      }
+   });
+}
--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const bodyParser = require('body-parser');
 
 var app = express();
 
-var registerController = require('./controller/register-controller');
+var userController = require('./controller/user-controller');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-app.post('/api/register',registerController.register);
+app.post('/api/register',userController.register);
+app.get('/api/getByUserRole',userController.getByUserRole);
 
 app.listen(3030);
